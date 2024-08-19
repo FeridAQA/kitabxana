8b10b822bb0360991e26629545af3f5cc731a4d6
ferid aqa <feridcodee@gmail.com>
2024-08-19 18:57:38 +0400

    validation yazıldı

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,10 +1,11 @@
 const express =require('express')
 const { C_login, C_createUser } = require('../controller/auth.controller')
 const loginLimiter = require('../middleware/loginLimiter')
+const registerValidation = require('../middleware/registerValidation')
 const router=express.Router()
 
 router.post('/login',loginLimiter,C_login)
-router.post('/register',C_createUser)
+router.post('/register',registerValidation,C_createUser)
 
 
 module.exports=router
